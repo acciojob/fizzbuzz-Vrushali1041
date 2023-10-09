8924adfe0bccd5e75a2152132b7249c0a7a9ad1c
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-10-09 13:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("runFizzBuzz");
+    const output = document.getElementById("output");
+
+    button.addEventListener("click", function () {
+        output.innerHTML = ""; // Clear previous results
+
+        for (let i = 1; i <= 100; i++) {
+            let result = "";
+
+            if (i % 3 === 0) {
+                result += "Fizz";
+            }
+            if (i % 5 === 0) {
+                result += "Buzz";
+            }
+            if (result === "") {
+                result = i.toString();
+            }
+
+            const div = document.createElement("div");
+            div.textContent = result;
+            output.appendChild(div);
+        }
+    });
+});
